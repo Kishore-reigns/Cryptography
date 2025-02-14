5a5d46184a816220c3d0dacf4d3816a5637a37ca
Kishore-reigns <112459807+Kishore-reigns@users.noreply.github.com>
2025-02-14 18:02:57 +0530

    some changes made

--- Shift_cipher/bruteforce.py
+++ Shift_cipher/bruteforce.py
@@ -1,6 +1,7 @@
 from shiftCipher import decrypt
 
 c = input("Enter the cipher text : ")
+keysize = int(input("Enter the largest key : "))
 
-for i in range(1,26) :
+for i in range(1,keysize+1) :
     print("\nkey : ", i , "plain text : ", decrypt(c,i))
