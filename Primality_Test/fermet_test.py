78525ef63856ccadd15d4fb4748b71d7cc4aecd1
Kishore-reigns <112459807+Kishore-reigns@users.noreply.github.com>
2025-03-19 05:56:32 +0530

    Added fermet_test for primality test

---
+++ Primality_Test/fermet_test.py
@@ -0,0 +1,10 @@
+
+def fermet_test(p):
+    for a in range(1,p):
+        if( ( (a**p) - a) % p != 0 ):
+            return False
+    return True
+
+if __name__ == "__main__":
+    p = int(input("Enter the number : "))
+    print(fermet_test(p))
